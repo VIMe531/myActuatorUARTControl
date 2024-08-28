fa99010a1cae171de438d2c37ba89257cb8c87ae
razervim <kotaroe531n@gmail.com>
2024-08-28 10:37:48 +0900

    update source files

--- test/UART_Motor_control_test.cpp
+++ test/UART_Motor_control_test.cpp
@@ -8,11 +8,11 @@
 
 class CRC16 {
 public:
-    static uint16_t calculate(const uint8_t* data, size_t start, size_t length) {
-        uint16_t crc = 0xFFFF;
+    static int16_t calculate(const int8_t* data, size_t start, size_t length) {
+        int16_t crc = 0xFFFF;
         for (size_t i = start; i < length; ++i) {
             crc ^= data[i];
-            for (uint8_t j = 0; j < 8; ++j) {
+            for (int8_t j = 0; j < 8; ++j) {
                 if (crc & 1)
                     crc = (crc >> 1) ^ 0xA001;
                 else
@@ -24,11 +24,11 @@ public:
 };
 
 struct Serialmessage{
-    uint8_t header = 0x3E;
-    uint8_t ID;
-    std::vector<uint8_t> data{8};
-    uint8_t data_length;
-    uint16_t crc;
+    int8_t header = 0x3E;
+    int8_t ID;
+    std::vector<int8_t> data{8};
+    int8_t data_length;
+    int16_t crc;
 };
 
 void handleSerialPortError(const std::string& message) {
@@ -46,7 +46,7 @@ void handleOpenError(HANDLE serialPort) {
     }
 }
 
-void handleWriteError(HANDLE serialPort, std::vector<uint8_t> message, DWORD bytes_written) {
+void handleWriteError(HANDLE serialPort, std::vector<int8_t> message, DWORD bytes_written) {
     if(!WriteFile(serialPort, message.data(), message.size(), &bytes_written, NULL)){
         handleSerialPortError("Error Writing to serial port");
         CloseHandle(serialPort);
@@ -63,7 +63,7 @@ void handleWriteError(HANDLE serialPort, std::vector<uint8_t> message, DWORD byt
     }
 }
 
-uint32_t handleReadError(HANDLE serialPort, std::vector<uint8_t> message, DWORD total_bytes_read, DWORD bytes_read, uint32_t target) {
+std::vector<int8_t> Readhandler(HANDLE serialPort, std::vector<int8_t> message, DWORD total_bytes_read, DWORD bytes_read, std::vector<int8_t> target) {
     while (total_bytes_read < message.size()) {
         if (!ReadFile(serialPort, message.data() + total_bytes_read, message.size() - total_bytes_read, &bytes_read, NULL)) {
             DWORD error = GetLastError();
@@ -77,7 +77,6 @@ uint32_t handleReadError(HANDLE serialPort, std::vector<uint8_t> message, DWORD 
     }
     if (total_bytes_read == message.size()) {
         std::cout << "Bytes read from serial port: " << total_bytes_read << std::endl;
-        target = (static_cast<uint16_t>(message[10]) << 24) | (static_cast<uint16_t>(message[9]) << 16) | (static_cast<uint16_t>(message[8]) << 8) | static_cast<uint16_t>(message[7]);
         std::cout << "Received data: ";
         for (DWORD i = 0; i < total_bytes_read; ++i) {
             std::cout << std::hex << static_cast<int>(message[i]) << " ";
@@ -87,7 +86,7 @@ uint32_t handleReadError(HANDLE serialPort, std::vector<uint8_t> message, DWORD 
     else {
         handleSerialPortError("Failed to read complete message");
     }
-    return target;
+    return message;
 }
 
 void handleTimeoutsError(HANDLE serialPort, COMMTIMEOUTS timeouts) {
@@ -117,8 +116,8 @@ void handleSetCommStateError(HANDLE serialPort, DCB dcbSerialParams) {
     else;
 }
 
-uint32_t SerialCommu(LPCWSTR port_name, DCB dcbSerialParams, COMMTIMEOUTS timeouts, std::vector<uint8_t> message) {
-    uint32_t target = 0x00000000;
+std::vector<int8_t> SerialCommu(LPCWSTR port_name, DCB dcbSerialParams, COMMTIMEOUTS timeouts, std::vector<int8_t> message) {
+    std::vector<int8_t> target = {0};
 
     HANDLE serialPort = CreateFileW(port_name, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
     dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
@@ -140,13 +139,13 @@ uint32_t SerialCommu(LPCWSTR port_name, DCB dcbSerialParams, COMMTIMEOUTS timeou
     handleTimeoutsError(serialPort, timeouts);
     handleWriteError(serialPort, message, bytes_written);
     PurgeComm(serialPort, PURGE_RXCLEAR);
-    target = handleReadError(serialPort, message, total_bytes_read, bytes_read, target);
+    target = Readhandler(serialPort, message, total_bytes_read, bytes_read, target);
     CloseHandle(serialPort);
     
     return target;
 }
 
-uint32_t SetAccel(uint8_t function_idx) {
+int32_t SetAccel(int8_t function_idx) {
     Serialmessage s_message;
     s_message.ID = 1;
     s_message.data[0] = 0x43;
@@ -158,9 +157,10 @@ uint32_t SetAccel(uint8_t function_idx) {
     s_message.data[6] = 0x00;
     s_message.data[7] = 0x00;
     s_message.data_length = DATA_LENGTH;
-    uint32_t Accel = 0x00000000;
+    std::vector<int8_t> Accel_message = {0};
+    int32_t Accel = 0x00000000;
 
-    std::vector<uint8_t> message;
+    std::vector<int8_t> message;
     message.push_back(s_message.header);
     message.push_back(s_message.ID);
     message.push_back(s_message.data_length);
@@ -173,7 +173,7 @@ uint32_t SetAccel(uint8_t function_idx) {
     message.push_back(s_message.data[6]);
     message.push_back(s_message.data[7]);
     // re-calculate CRC
-    uint16_t crc = CRC16::calculate(message.data(), 0, message.size());
+    int16_t crc = CRC16::calculate(message.data(), 0, message.size());
     message.push_back(crc & 0xFF);
     message.push_back((crc >> 8) & 0xFF);
 
@@ -181,11 +181,12 @@ uint32_t SetAccel(uint8_t function_idx) {
     DCB dcbSerialParams = { 0 };
     COMMTIMEOUTS timeouts = { 0 };
     LPCWSTR port_name = L"\\\\.\\COM34";
-    Accel = SerialCommu(port_name, dcbSerialParams, timeouts, message);
+    Accel_message = SerialCommu(port_name, dcbSerialParams, timeouts, message);
+    Accel = (static_cast<uint8_t>(Accel_message[7]) << 24) | (static_cast<uint8_t>(Accel_message[6]) << 16) | (static_cast<uint8_t>(Accel_message[5]) << 8) | (static_cast<uint8_t>(Accel_message[4]));
     return Accel;
 }
 
-uint16_t ReadAccel(uint8_t function_idx) {
+int32_t ReadAccel(int8_t function_idx) {
     Serialmessage s_message;
     s_message.ID = 1;
     s_message.data[0] = 0x42;
@@ -197,9 +198,10 @@ uint16_t ReadAccel(uint8_t function_idx) {
     s_message.data[6] = 0x00;
     s_message.data[7] = 0x00;
     s_message.data_length = DATA_LENGTH;
-    uint32_t Accel = 0x00000000;
+    std::vector<int8_t> Accel_message = {0};
+    int32_t Accel = 0x00000000;
 
-    std::vector<uint8_t> message;
+    std::vector<int8_t> message;
     message.push_back(s_message.header);
     message.push_back(s_message.ID);
     message.push_back(s_message.data_length);
@@ -212,7 +214,7 @@ uint16_t ReadAccel(uint8_t function_idx) {
     message.push_back(s_message.data[6]);
     message.push_back(s_message.data[7]);
     // re-calculate CRC
-    uint16_t crc = CRC16::calculate(message.data(), 0, message.size());
+    int16_t crc = CRC16::calculate(message.data(), 0, message.size());
     message.push_back(crc & 0xFF);
     message.push_back((crc >> 8) & 0xFF);
 
@@ -220,11 +222,12 @@ uint16_t ReadAccel(uint8_t function_idx) {
     DCB dcbSerialParams = { 0 };
     COMMTIMEOUTS timeouts = { 0 };
     LPCWSTR port_name = L"\\\\.\\COM34";
-    Accel = SerialCommu(port_name, dcbSerialParams, timeouts, message);
+    Accel_message = SerialCommu(port_name, dcbSerialParams, timeouts, message);
+    Accel = (static_cast<uint8_t>(Accel_message[7]) << 24) | (static_cast<uint8_t>(Accel_message[6]) << 16) | (static_cast<uint8_t>(Accel_message[5]) << 8) | (static_cast<uint8_t>(Accel_message[4]));
     return Accel;
 }
 
-uint16_t ReadSingleAngle(void) {
+int16_t ReadSingleAngle(void) {
     Serialmessage s_message;
     s_message.ID = 1;
     s_message.data[0] = 0x94;
@@ -236,9 +239,10 @@ uint16_t ReadSingleAngle(void) {
     s_message.data[6] = 0x00;
     s_message.data[7] = 0x00;
     s_message.data_length = DATA_LENGTH;
-    uint16_t AbsAngle = 0x0000;
+    std::vector<int8_t> AbsAngle_message = {0};
+    int16_t AbsAngle = 0;
 
-    std::vector<uint8_t> message;
+    std::vector<int8_t> message;
     message.push_back(s_message.header);
     message.push_back(s_message.ID);
     message.push_back(s_message.data_length);
@@ -250,89 +254,68 @@ uint16_t ReadSingleAngle(void) {
     message.push_back(s_message.data[5]);
     message.push_back(s_message.data[6]);
     message.push_back(s_message.data[7]);
-    // re-calculate CRC
-    uint16_t crc = CRC16::calculate(message.data(), 0, message.size());
-    message.push_back(crc & 0xFF);
-    message.push_back((crc >> 8) & 0xFF);
 
     HANDLE serialPort;
     DCB dcbSerialParams = { 0 };
     COMMTIMEOUTS timeouts = { 0 };
     LPCWSTR port_name = L"\\\\.\\COM34";
     serialPort = CreateFileW(port_name, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-    if (serialPort == INVALID_HANDLE_VALUE) {
-        std::cerr << "Error in opening serial port" << std::endl;
-    } else {
-        std::cout << "Opening serial port successful" << std::endl;
+    // re-calculate CRC
+    int16_t crc = CRC16::calculate(message.data(), 0, message.size());
+    message.push_back(crc & 0xFF);
+    message.push_back((crc >> 8) & 0xFF);
 
-        dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
-        if (!GetCommState(serialPort, &dcbSerialParams)) {
-            std::cerr << "Error getting serial port state" << std::endl;
-        } else {
-            dcbSerialParams.BaudRate = CBR_115200;
-            dcbSerialParams.ByteSize = 8;
-            dcbSerialParams.StopBits = ONESTOPBIT;
-            dcbSerialParams.Parity = NOPARITY;
-            if (!SetCommState(serialPort, &dcbSerialParams)) {
-                std::cerr << "Error setting serial port state" << std::endl;
-            } else {
-                timeouts.ReadIntervalTimeout = 50;
-                timeouts.ReadTotalTimeoutConstant = 50;
-                timeouts.ReadTotalTimeoutMultiplier = 10;
-                timeouts.WriteTotalTimeoutConstant = 50;
-                timeouts.WriteTotalTimeoutMultiplier = 10;
-                if (!SetCommTimeouts(serialPort, &timeouts)) {
-                    std::cerr << "Error setting timeouts" << std::endl;
-                } else {
-                    DWORD bytes_written, bytes_read;
-                    if (!WriteFile(serialPort, message.data(), message.size(), &bytes_written, NULL)) {
-                        std::cerr << "Error writing to serial port" << std::endl;
-                    } else {
-                        std::cout << "Bytes written to serial port: " << bytes_written << std::endl;
-                        std::cout << "written data : ";
-                        for (DWORD i = 0; i < bytes_written; ++i) {
-                            std::cout << std::hex << static_cast<int>(message[i]) << " ";
-                        }
-                        std::cout << std::dec << std::endl;
-                        
-                        PurgeComm(serialPort, PURGE_RXCLEAR);
-                        
-                        DWORD total_bytes_read = 0;
-                        while (total_bytes_read < message.size()) {
-                            if (!ReadFile(serialPort, message.data() + total_bytes_read, message.size() - total_bytes_read, &bytes_read, NULL)) {
-                                DWORD error = GetLastError();
-                                std::cerr << "ReadFile failed with error " << error << std::endl;
-                                break;
-                            }
-                            total_bytes_read += bytes_read;
-                        }
-
-                        if (total_bytes_read == message.size()) {
-                            std::cout << "Bytes read from serial port: " << total_bytes_read << std::endl;
-                            AbsAngle = (static_cast<uint16_t>(message[10]) << 8) | static_cast<uint16_t>(message[9]);
-                            std::cout << "Received data: ";
-                            for (DWORD i = 0; i < total_bytes_read; ++i) {
-                                std::cout << std::hex << static_cast<int>(message[i]) << " ";
-                            }
-                            std::cout << std::dec << std::endl;
-                        } else {
-                            std::cerr << "Failed to read complete message" << std::endl;
-                        }
-                    }
-                }
-            }
-        }
-        CloseHandle(serialPort);
-    }
+    AbsAngle_message = SerialCommu(port_name, dcbSerialParams, timeouts, message);
+    AbsAngle = (static_cast<uint8_t>(AbsAngle_message[7]) << 8) | (static_cast<uint8_t>(AbsAngle_message[6]));
     return AbsAngle;
 }
 
+void SinglePositionCtrl(uint8_t spinDirection,uint16_t maxspeed, uint16_t angleControl){
+    Serialmessage s_message;
+    s_message.ID = 1;
+    s_message.data[0] = 0xA6;
+    s_message.data[1] = spinDirection;  // 0x00 (CW) or 0x01 (CCW)
+    s_message.data[2] = static_cast<uint8_t>(maxspeed & 0x00FF);
+    s_message.data[3] = static_cast<uint8_t>(maxspeed & 0xFF00);
+    s_message.data[4] = static_cast<uint8_t>(angleControl & 0x00FF);
+    s_message.data[5] = static_cast<uint8_t>(angleControl & 0xFF00);
+    s_message.data[6] = 0x00;
+    s_message.data[7] = 0x00;
+    s_message.data_length = DATA_LENGTH;
+    std::vector<int8_t> AbsAngle_message = {0};
+    int16_t AbsAngle = 0;
+
+    std::vector<int8_t> message;
+    message.push_back(s_message.header);
+    message.push_back(s_message.ID);
+    message.push_back(s_message.data_length);
+    message.push_back(s_message.data[0]);
+    message.push_back(s_message.data[1]);
+    message.push_back(s_message.data[2]);
+    message.push_back(s_message.data[3]);
+    message.push_back(s_message.data[4]);
+    message.push_back(s_message.data[5]);
+    message.push_back(s_message.data[6]);
+    message.push_back(s_message.data[7]);
+
+    HANDLE serialPort;
+    DCB dcbSerialParams = { 0 };
+    COMMTIMEOUTS timeouts = { 0 };
+    LPCWSTR port_name = L"\\\\.\\COM34";
+    serialPort = CreateFileW(port_name, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    // re-calculate CRC
+    int16_t crc = CRC16::calculate(message.data(), 0, message.size());
+    message.push_back(crc & 0xFF);
+    message.push_back((crc >> 8) & 0xFF);
+
+    AbsAngle_message = SerialCommu(port_name, dcbSerialParams, timeouts, message);
+}
 
 int main(void) {
 
-    uint16_t Angle = ReadSingleAngle();
-    uint32_t get_Accel = ReadAccel(0x00);
-    uint32_t in_Accel = SetAccel(0x00);
+    int16_t Angle = ReadSingleAngle();
+    int32_t get_Accel = ReadAccel(0x00);
+    int32_t in_Accel = SetAccel(0x00);
     std::cout << "Motor Angle : " << static_cast<int>(Angle) << std::endl;
     std::cout << "Motor Measured Accel : " << get_Accel << std::endl;
     std::cout << "Motor Input Accel : " << in_Accel << std::endl;
